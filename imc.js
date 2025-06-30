17cf5cb8a67307135b2ccb061cc19f0ab9e45506
Arthur <arthuralexandres.ti@gmail.com>
2025-06-30 00:39:16 -0300

    feat: Cria os arquivos principais, adiociona lógica simples de cálculo de IMC e estrutura HTML básica

---
+++ imc.js
@@ -0,0 +1,15 @@
+function Imc(name, age, weight, height) {
+  this.name = name;
+  this.age = age;
+  this.weight = weight;
+  this.height = height;
+  this.imc_calculator = () => {
+    let calculate_imc = this.weight / (this.height * this.height);
+    return calculate_imc.toFixed(2);
+  };
+}
+
+(function p1() {
+  let p1 = new Imc("Arthur", 25, 80, 1.8);
+  console.log(p1.imc_calculator());
+})();
